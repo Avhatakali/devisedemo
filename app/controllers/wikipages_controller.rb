ea51365ac9952833f3b3e42f1259a4378df56da5
Avhatakali Makwarela <avhatakali@Avhatakalis-iMac.local>
2019-02-21 16:30:38 +0200

    add a branch

---
+++ app/controllers/wikipages_controller.rb
@@ -0,0 +1,2 @@
+class WikipagesController < ApplicationController
+end
---
+++ test/controllers/wikipages_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WikipagesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
